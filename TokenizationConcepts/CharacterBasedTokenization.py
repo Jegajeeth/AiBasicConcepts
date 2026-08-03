893f62fd32d2d7f29991736f9a999b5e2bb76485
jegajeeth <jegajeeth2019@gmail.com>
2026-08-04 00:32:33 +0530

    add(tokenconcept): added a small project about character based tokenization and virtualized the tokenized text in a graph.

---
+++ TokenizationConcepts/CharacterBasedTokenization.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as matplotlib
+
+
+txt = "this is a test text generated to understand the workings of the tokenization concept."
+# txt = "The way you do anything is the way you do everything.";
+
+
+# toknization using dictanary O(n)
+char_vocab = {};
+i = 0;
+
+for c in txt:
+    if(char_vocab.get(c) == None):
+        char_vocab[c] = i;
+        i += 1;
+print("Token :", end=" ");
+for cvk in char_vocab.keys():
+    print(f" \"{cvk}\" |", end="");
+print();
+print("-" * (i * 8));
+print("Index:", end=" ");
+for cvi in char_vocab.values():
+    print(f" \"{cvi}\" |", end="");
+print();
+
+tokenizedTxt = [char_vocab[char] for char in txt];
+print (tokenizedTxt);
+
+# tokenization using list and sorting O(n log n)
+# characters = [char for char in txt];
+# char_vocab = list(sorted(set(characters)));
+
+# print(char_vocab);
+
+# tokenizedTxt = [char_vocab.index(char) for char in txt];
+# print(tokenizedTxt);
+
+_,ax = matplotlib.subplots();
+ax.plot(tokenizedTxt, 'o', markersize=10, markerfacecolor=[.0,.0,.0], linestyle="--");
+# ax.set(xlable="Txt tokens");
+ax.grid(visible=True, axis="y");
+matplotlib.yticks(range(len(char_vocab)));
+
+ax2 = ax.twinx()
+ax2.plot(tokenizedTxt,alpha=0)
+
+# matplotlib.plot(xpoints, ypoints);
+matplotlib.ylabel("vocab_text");
+matplotlib.xlabel("txt_tokens");
+matplotlib.yticks(range(len(char_vocab)), labels=list(char_vocab.keys()));
+matplotlib.show();
+
+
